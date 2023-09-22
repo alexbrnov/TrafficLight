296e440db6be14ba762be9ce88075cb94a00765c
Alexandr Baranov <alexbrnov@gmail.com>
2023-09-22 23:54:32 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Alexandr Baranov on 22.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
