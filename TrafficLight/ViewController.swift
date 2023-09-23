c192cff82bb030e72722394a54ae8540d485dbcf
Alexandr Baranov <alexbrnov@gmail.com>
2023-09-23 23:13:59 +0300

    done

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
 
+final class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOff: CGFloat = 0.3
+    private let lightIsOn: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        switchButton.layer.cornerRadius = 6
     }
-
-
+    
+    @IBAction func switchButtonTapped() {
+        switchButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
 }
 
